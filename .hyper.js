68d2fdc461a6141209d3095e8e8c78a9a5d83a8d
German Rodriguez <germanson@gmail.com>
2017-11-06 13:07:08 -0700

    Changing themes

--- .hyper.js
+++ .hyper.js
@@ -111,7 +111,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-chesterish', 'hyper-blink', 'hyper-tabs-enhanced'],
+  plugins: ['hyper-oceans16', 'hyper-blink', 'hyper-tabs-enhanced'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
